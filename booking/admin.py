e0d51e40d675091d16741ca7b971b834302ffc97
Bảo Toàn Xuân Nguyễn <nxbtoan175@gmail.com>
2025-11-18 13:42:51 +0700

    Dang ky models vao Admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Room, Booking
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'shopify_product_id', 'max_guests', 'area')
+    search_fields = ('name', 'shopify_product_id')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('room', 'start_date', 'end_date', 'customer_email', 'status', 'shopify_order_id')
+    list_filter = ('status', 'room')
+    search_fields = ('customer_email', 'shopify_order_id')
