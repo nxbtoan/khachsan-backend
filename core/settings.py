7a51819bd5dfaa8df1bbef9c3b3814e8a73af810
Bảo Toàn Xuân Nguyễn <nxbtoan175@gmail.com>
2025-11-18 09:40:06 +0700

    Cau hinh SSL cho database Neon

--- core/settings.py
+++ core/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 }
 
